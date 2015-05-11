c90a50d6f42955bc30d007613675644ae1117106
Lars Schütze <dev@larsschuetze.de>
2015-05-11 15:33:40 +0200

    Formatted source code and organized imports

--- plugins/de.tudresden.slr.ui.chart/src/helloworldchart/Bubble.java
+++ plugins/de.tudresden.slr.ui.chart/src/helloworldchart/Bubble.java
@@ -42,118 +42,110 @@ import org.eclipse.birt.chart.model.type.impl.BubbleSeriesImpl;
  * 
  */
 
-public class Bubble
-{
+public class Bubble {
 
-	public final static Chart createBubble( )
-	{
-	
-		ChartWithAxes cwaBubble = ChartWithAxesImpl.create( );
-		
-//		cwaBubble.setScript( "function beforeDrawAxisTitle(axis, title, scriptContext)" //$NON-NLS-1$
-//				+ "{title.getCaption( ).setValue( \"Axis Title By JavaScript\");" //$NON-NLS-1$
-//				+ "title.getCaption( ).getColor( ).set( 32, 168, 255 );}" //$NON-NLS-1$
-//		);
-		
-		cwaBubble.setScript( "function beforeDrawAxisLabel(axis, label, scriptContext)" //$NON-NLS-1$
-				+ "{if (label.getCaption( ).getValue( ) == 10)"
-				+ "{label.getCaption().setValue( \"Apfel\" )}"
-				+ "label.getCaption( ).getFont( ).setSize(7);" 
-				+ "}\n"		
-				);
-				
-		cwaBubble.setType( "Bubble Chart" ); //$NON-NLS-1$
-		cwaBubble.setSubType( "Standard Bubble Chart" ); //$NON-NLS-1$
+	public final static Chart createBubble() {
+
+		ChartWithAxes cwaBubble = ChartWithAxesImpl.create();
+
+		//		cwaBubble.setScript( "function beforeDrawAxisTitle(axis, title, scriptContext)" //$NON-NLS-1$
+		//				+ "{title.getCaption( ).setValue( \"Axis Title By JavaScript\");" //$NON-NLS-1$
+		//				+ "title.getCaption( ).getColor( ).set( 32, 168, 255 );}" //$NON-NLS-1$
+		// );
+
+		cwaBubble
+				.setScript("function beforeDrawAxisLabel(axis, label, scriptContext)" //$NON-NLS-1$
+						+ "{if (label.getCaption( ).getValue( ) == 10)"
+						+ "{label.getCaption().setValue( \"Apfel\" )}"
+						+ "label.getCaption( ).getFont( ).setSize(7);" + "}\n");
+
+		cwaBubble.setType("Bubble Chart"); //$NON-NLS-1$
+		cwaBubble.setSubType("Standard Bubble Chart"); //$NON-NLS-1$
 		// Plot
-		cwaBubble.getBlock( ).setBackground( ColorDefinitionImpl.WHITE( ) );
-		cwaBubble.getBlock( ).getOutline( ).setVisible( true );
-		Plot p = cwaBubble.getPlot( );
-		p.getClientArea( ).setBackground( ColorDefinitionImpl.create( 255,
-				255,
-				225 ) );
+		cwaBubble.getBlock().setBackground(ColorDefinitionImpl.WHITE());
+		cwaBubble.getBlock().getOutline().setVisible(true);
+		Plot p = cwaBubble.getPlot();
+		p.getClientArea().setBackground(
+				ColorDefinitionImpl.create(255, 255, 225));
 
 		// Title
-		cwaBubble.getTitle( )
-				.getLabel( )
-				.getCaption( )
-				.setValue( "Bubble Chart" ); //$NON-NLS-1$
+		cwaBubble.getTitle().getLabel().getCaption().setValue("Bubble Chart"); //$NON-NLS-1$
 
 		// Legend
-		Legend lg = cwaBubble.getLegend( );
-		lg.setItemType( LegendItemType.SERIES_LITERAL );
+		Legend lg = cwaBubble.getLegend();
+		lg.setItemType(LegendItemType.SERIES_LITERAL);
 
 		// X-Axis
-		Axis xAxisPrimary = cwaBubble.getPrimaryBaseAxes( )[0];
+		Axis xAxisPrimary = cwaBubble.getPrimaryBaseAxes()[0];
 
-		xAxisPrimary.setType( AxisType.TEXT_LITERAL );
-		xAxisPrimary.getMajorGrid( ).setTickStyle( TickStyle.BELOW_LITERAL );
-		xAxisPrimary.getOrigin( ).setType( IntersectionType.MIN_LITERAL );
-				
+		xAxisPrimary.setType(AxisType.TEXT_LITERAL);
+		xAxisPrimary.getMajorGrid().setTickStyle(TickStyle.BELOW_LITERAL);
+		xAxisPrimary.getOrigin().setType(IntersectionType.MIN_LITERAL);
+
 		// Y-Axis
-		Axis yAxisPrimary = cwaBubble.getPrimaryOrthogonalAxis( xAxisPrimary );
-		yAxisPrimary.getMajorGrid( ).setTickStyle( TickStyle.LEFT_LITERAL );
-		yAxisPrimary.setType( AxisType.LINEAR_LITERAL );
-//		yAxisPrimary.getLabel( ).getCaption( ).getFont( ).setRotation( 90 );
-		yAxisPrimary.getLabel( ).getCaption( ).getFont( ).setWordWrap(true);;
+		Axis yAxisPrimary = cwaBubble.getPrimaryOrthogonalAxis(xAxisPrimary);
+		yAxisPrimary.getMajorGrid().setTickStyle(TickStyle.LEFT_LITERAL);
+		yAxisPrimary.setType(AxisType.LINEAR_LITERAL);
+		// yAxisPrimary.getLabel( ).getCaption( ).getFont( ).setRotation( 90 );
+		yAxisPrimary.getLabel().getCaption().getFont().setWordWrap(true);
+		;
 
 		// Data Set
-//		NumberDataSet categoryValues = NumberDataSetImpl.create( new double[] {
-//				20,45,70,100,120,130
-//		});
-		TextDataSet categoryValues = TextDataSetImpl.create( new String[]{
-				"Birne", "Gurke"} );
-		BubbleDataSet values1 = BubbleDataSetImpl.create( new BubbleEntry[]{
-				new BubbleEntry( Integer.valueOf( 10 ), Integer.valueOf( 100 ) ),
-				new BubbleEntry( Integer.valueOf( 20 ), Integer.valueOf( 200 ) )
-				
-				
-		} );
-		BubbleDataSet values2 = BubbleDataSetImpl.create( new BubbleEntry[]{
-				new BubbleEntry( Integer.valueOf( 50 ), Integer.valueOf( 60 ) ),
-				null,
-				null,
-				new BubbleEntry( Integer.valueOf( 43 ), Integer.valueOf( 80 ) ),
-				new BubbleEntry( Integer.valueOf( 12 ), Integer.valueOf( 100 ) ),
-				null
-		} );
+		// NumberDataSet categoryValues = NumberDataSetImpl.create( new double[]
+		// {
+		// 20,45,70,100,120,130
+		// });
+		TextDataSet categoryValues = TextDataSetImpl.create(new String[] {
+				"Birne", "Gurke" });
+		BubbleDataSet values1 = BubbleDataSetImpl.create(new BubbleEntry[] {
+				new BubbleEntry(Integer.valueOf(10), Integer.valueOf(100)),
+				new BubbleEntry(Integer.valueOf(20), Integer.valueOf(200))
 
-		SampleData sd = DataFactory.eINSTANCE.createSampleData( );
-		BaseSampleData sdBase = DataFactory.eINSTANCE.createBaseSampleData( );
-		sdBase.setDataSetRepresentation( "" );//$NON-NLS-1$
-		sd.getBaseSampleData( ).add( sdBase );
+		});
+		BubbleDataSet values2 = BubbleDataSetImpl.create(new BubbleEntry[] {
+				new BubbleEntry(Integer.valueOf(50), Integer.valueOf(60)),
+				null, null,
+				new BubbleEntry(Integer.valueOf(43), Integer.valueOf(80)),
+				new BubbleEntry(Integer.valueOf(12), Integer.valueOf(100)),
+				null });
 
-		OrthogonalSampleData sdOrthogonal1 = DataFactory.eINSTANCE.createOrthogonalSampleData( );
-		sdOrthogonal1.setDataSetRepresentation( "" );//$NON-NLS-1$
-		sdOrthogonal1.setSeriesDefinitionIndex( 0 );
-		sd.getOrthogonalSampleData( ).add( sdOrthogonal1 );
+		SampleData sd = DataFactory.eINSTANCE.createSampleData();
+		BaseSampleData sdBase = DataFactory.eINSTANCE.createBaseSampleData();
+		sdBase.setDataSetRepresentation("");//$NON-NLS-1$
+		sd.getBaseSampleData().add(sdBase);
 
-		cwaBubble.setSampleData( sd );
+		OrthogonalSampleData sdOrthogonal1 = DataFactory.eINSTANCE
+				.createOrthogonalSampleData();
+		sdOrthogonal1.setDataSetRepresentation("");//$NON-NLS-1$
+		sdOrthogonal1.setSeriesDefinitionIndex(0);
+		sd.getOrthogonalSampleData().add(sdOrthogonal1);
+
+		cwaBubble.setSampleData(sd);
 
 		// X-Series
-		Series seCategory = SeriesImpl.create( );
-		seCategory.setDataSet( categoryValues );
+		Series seCategory = SeriesImpl.create();
+		seCategory.setDataSet(categoryValues);
 
-		SeriesDefinition sdX = SeriesDefinitionImpl.create( );
-		sdX.getSeriesPalette( ).shift( 0 );
-		xAxisPrimary.getSeriesDefinitions( ).add( sdX );
-		sdX.getSeries( ).add( seCategory );
+		SeriesDefinition sdX = SeriesDefinitionImpl.create();
+		sdX.getSeriesPalette().shift(0);
+		xAxisPrimary.getSeriesDefinitions().add(sdX);
+		sdX.getSeries().add(seCategory);
 
 		// Y-Series
-		BubbleSeries bs1 = (BubbleSeries) BubbleSeriesImpl.create( );
-		bs1.setDataSet( values1 );
-		bs1.getLabel( ).setVisible( false );
-		
-		BubbleSeries bs2 = (BubbleSeries) BubbleSeriesImpl.create( );
-		bs2.setDataSet( values2 );
-		bs2.getLabel( ).setVisible( false );
-		
-		
-		SeriesDefinition sdY = SeriesDefinitionImpl.create( );
-		sdY.getSeriesPalette( ).shift( -1 );
-		yAxisPrimary.getSeriesDefinitions( ).add( sdY );
-		sdY.getSeries( ).add( bs1 );
-//		sdY.getSeries( ).add( bs2 );
-//		sdY.getSeries( ).add( bs3 );
+		BubbleSeries bs1 = (BubbleSeries) BubbleSeriesImpl.create();
+		bs1.setDataSet(values1);
+		bs1.getLabel().setVisible(false);
+
+		BubbleSeries bs2 = (BubbleSeries) BubbleSeriesImpl.create();
+		bs2.setDataSet(values2);
+		bs2.getLabel().setVisible(false);
+
+		SeriesDefinition sdY = SeriesDefinitionImpl.create();
+		sdY.getSeriesPalette().shift(-1);
+		yAxisPrimary.getSeriesDefinitions().add(sdY);
+		sdY.getSeries().add(bs1);
+		// sdY.getSeries( ).add( bs2 );
+		// sdY.getSeries( ).add( bs3 );
 
 		return cwaBubble;
 	}
--- plugins/de.tudresden.slr.ui.chart/src/helloworldchart/HelloWorldView.java
+++ plugins/de.tudresden.slr.ui.chart/src/helloworldchart/HelloWorldView.java
@@ -1,32 +1,14 @@
 package helloworldchart;
 
 import org.eclipse.birt.chart.api.ChartEngine;
-import org.eclipse.birt.chart.computation.DataPointHints;
-import org.eclipse.birt.chart.computation.LegendEntryRenderingHints;
-import org.eclipse.birt.chart.computation.PlotComputation;
-import org.eclipse.birt.chart.datafeed.IDataSetProcessor;
 import org.eclipse.birt.chart.device.IDeviceRenderer;
 import org.eclipse.birt.chart.exception.ChartException;
-import org.eclipse.birt.chart.factory.GeneratedChartState;
 import org.eclipse.birt.chart.factory.Generator;
 import org.eclipse.birt.chart.model.Chart;
 import org.eclipse.birt.chart.model.ChartWithAxes;
 import org.eclipse.birt.chart.model.attribute.Bounds;
-import org.eclipse.birt.chart.model.attribute.Fill;
-import org.eclipse.birt.chart.model.attribute.Marker;
 import org.eclipse.birt.chart.model.attribute.impl.BoundsImpl;
-import org.eclipse.birt.chart.model.component.Axis;
-import org.eclipse.birt.chart.model.component.CurveFitting;
-import org.eclipse.birt.chart.model.component.Label;
-import org.eclipse.birt.chart.model.component.MarkerLine;
-import org.eclipse.birt.chart.model.component.MarkerRange;
-import org.eclipse.birt.chart.model.component.Series;
-import org.eclipse.birt.chart.model.data.DataSet;
 import org.eclipse.birt.chart.model.impl.ChartWithAxesImpl;
-import org.eclipse.birt.chart.model.layout.Block;
-import org.eclipse.birt.chart.render.ISeriesRenderer;
-import org.eclipse.birt.chart.script.IChartEventHandler;
-import org.eclipse.birt.chart.script.IChartScriptContext;
 import org.eclipse.birt.core.framework.PlatformConfig;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
@@ -34,71 +16,64 @@ import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
-public class HelloWorldView extends ViewPart{
+public class HelloWorldView extends ViewPart {
 
 	public HelloWorldView() {
 	}
-	    private IDeviceRenderer idr = null;
-	    private Chart myChart = null;
-	    
+
+	private IDeviceRenderer idr = null;
+	private Chart myChart = null;
+
 	@Override
 	public void createPartControl(Composite parent) {
-		
+
 		// INITIALIZE THE SWT RENDERING DEVICE
-                
-		PlatformConfig config = new PlatformConfig( );
-				
+
+		PlatformConfig config = new PlatformConfig();
+
 		myChart = Bubble.createBubble();
-				//createMyChart(); 
-	    try {
-	            idr = ChartEngine.instance( config ).getRenderer("dv.SWT");
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	            //DefaultLoggerImpl.instance().log(e);
-	        }
-	  
-	   parent.addPaintListener(new PaintListener() {
-		
-		@Override
-		public void paintControl(PaintEvent pe) {
-			idr.setProperty(
-					IDeviceRenderer.GRAPHICS_CONTEXT, pe.gc);
-					Composite co = (Composite) pe.getSource();
-					Rectangle re = co.getClientArea();
-					Bounds bo = BoundsImpl.create(re.x, re.y,
-					re.width, re.height);
-					bo.scale( 72d /
-					idr.getDisplayServer().getDpiResolution()
-					); // BOUNDS MUST BE SPECIFIED IN POINTS
-					// BUILD AND RENDER THE CHART
-			 
-					Generator gr = Generator.instance();
-					try {
-						gr.render(idr, gr.build(idr.getDisplayServer(), myChart, bo, null));
-					} catch (ChartException gex)
-					{
-						gex.printStackTrace();
-					}
-			
+		// createMyChart();
+		try {
+			idr = ChartEngine.instance(config).getRenderer("dv.SWT");
+		} catch (Exception e) {
+			e.printStackTrace();
+			// DefaultLoggerImpl.instance().log(e);
 		}
-	});
 
+		parent.addPaintListener(new PaintListener() {
 
+			@Override
+			public void paintControl(PaintEvent pe) {
+				idr.setProperty(IDeviceRenderer.GRAPHICS_CONTEXT, pe.gc);
+				Composite co = (Composite) pe.getSource();
+				Rectangle re = co.getClientArea();
+				Bounds bo = BoundsImpl.create(re.x, re.y, re.width, re.height);
+				// BOUNDS MUST BE SPECIFIED IN POINTS
+				// BUILD AND RENDER THE CHART
+				bo.scale(72d / idr.getDisplayServer().getDpiResolution());
 
+				Generator gr = Generator.instance();
+				try {
+					gr.render(idr,
+							gr.build(idr.getDisplayServer(), myChart, bo, null));
+				} catch (ChartException gex) {
+					gex.printStackTrace();
+				}
 
+			}
+		});
 	}
 
 	@Override
 	public void setFocus() {
 
-
 	}
 
 	public static Chart createMyChart() {
 		ChartWithAxes cwaEmpty = ChartWithAxesImpl.create();
-		 
+
 		cwaEmpty.getTitle().getLabel().getCaption().setValue("Hello Chart");
-	 
-	    return cwaEmpty;
+
+		return cwaEmpty;
 	}
 }
