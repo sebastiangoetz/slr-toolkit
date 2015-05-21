24e7809e0cc5c394373c660b33e29c65a0e0a4e5
Lars Schütze <dev@larsschuetze.de>
2015-05-21 15:38:33 +0200

    Add new Taxonomy Check View to perspective

--- plugins/de.tudresden.slr.ui.perspective/src/de/tudresden/slr/ui/perspective/PerspectiveFactory.java
+++ plugins/de.tudresden.slr.ui.perspective/src/de/tudresden/slr/ui/perspective/PerspectiveFactory.java
@@ -4,6 +4,10 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+import bibtex.presentation.BibtexEditor;
+import bibtex.presentation.BibtexEntryEditor;
+import de.tudresden.slr.model.taxonomy.ui.views.TaxonomyCheckboxListView;
+
 public class PerspectiveFactory implements IPerspectiveFactory {
 
 	@Override
@@ -13,24 +17,25 @@ public class PerspectiveFactory implements IPerspectiveFactory {
 
 	private void defineLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
-		//layout.setEditorAreaVisible(true);
+		// layout.setEditorAreaVisible(true);
 
 		IFolderLayout left = layout.createFolder("slr.left", IPageLayout.LEFT,
 				0.25f, editorArea);
 		left.addView(IPageLayout.ID_PROJECT_EXPLORER);
-		
+		left.addView(BibtexEditor.ID);
+
 		IFolderLayout bottom = layout.createFolder("slr.bottom",
 				IPageLayout.BOTTOM, 0.65f, editorArea);
 		bottom.addView(IPageLayout.ID_PROP_SHEET);
 		bottom.addView(IPageLayout.ID_PROBLEM_VIEW);
 
-		IFolderLayout middle = layout.createFolder("slr.middle", IPageLayout.TOP,
-				0.3f, "slr.bottom");
-		middle.addView(IPageLayout.ID_EDITOR_AREA);
-//		middle.addPlaceholder("de.tudresden.slr.ui.bibtex.view");
+		IFolderLayout middle = layout.createFolder("slr.middle",
+				IPageLayout.TOP, 0.3f, "slr.bottom");
+		// middle.addPlaceholder(IPageLayout.ID_EDITOR_AREA);
+		middle.addPlaceholder(BibtexEntryEditor.ID);
 
-		IFolderLayout right = layout.createFolder("slr.right", IPageLayout.RIGHT,
-				0.75f, editorArea);
-		right.addView(IPageLayout.ID_OUTLINE);
+		IFolderLayout right = layout.createFolder("slr.right",
+				IPageLayout.RIGHT, 0.75f, editorArea);
+		right.addView(TaxonomyCheckboxListView.ID);
 	}
 }
