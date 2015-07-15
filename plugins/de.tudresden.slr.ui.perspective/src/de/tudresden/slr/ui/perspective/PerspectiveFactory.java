73611fd9faa91854a9b1d2643eb6490a5c8c7749
Lars Schuetze <dev@larsschuetze.de>
2015-07-15 12:17:27 +0200

    Move definition of views in the SLR perspective to plugin.xml extension points.
    Signed-off-by: Lars Schuetze <dev@larsschuetze.de>

--- plugins/de.tudresden.slr.ui.perspective/src/de/tudresden/slr/ui/perspective/PerspectiveFactory.java
+++ plugins/de.tudresden.slr.ui.perspective/src/de/tudresden/slr/ui/perspective/PerspectiveFactory.java
@@ -1,43 +1,11 @@
 package de.tudresden.slr.ui.perspective;
 
-import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
-
-import de.tudresden.slr.model.bibtex.ui.presentation.BibtexEditor;
-import de.tudresden.slr.model.bibtex.ui.presentation.BibtexEntryView;
-import de.tudresden.slr.model.bibtex.ui.presentation.BibtexOverviewEditor;
-import de.tudresden.slr.model.taxonomy.ui.views.TaxonomyCheckboxListView;
 
 public class PerspectiveFactory implements IPerspectiveFactory {
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
-		defineLayout(layout);
-	}
-
-	private void defineLayout(IPageLayout layout) {
-		String editorArea = layout.getEditorArea();
-		// layout.setEditorAreaVisible(true);
-
-		IFolderLayout left = layout.createFolder("slr.left", IPageLayout.LEFT,
-				0.25f, editorArea);
-		left.addView(IPageLayout.ID_PROJECT_EXPLORER);
-		left.addView(BibtexEntryView.ID);
-
-		IFolderLayout bottom = layout.createFolder("slr.bottom",
-				IPageLayout.BOTTOM, 0.65f, editorArea);
-		bottom.addView(IPageLayout.ID_PROP_SHEET);
-		bottom.addView(IPageLayout.ID_PROBLEM_VIEW);
-
-		IFolderLayout middle = layout.createFolder("slr.middle",
-				IPageLayout.TOP, 0.3f, "slr.bottom");
-		// middle.addPlaceholder(IPageLayout.ID_EDITOR_AREA);
-		middle.addPlaceholder(BibtexEditor.ID);
-		middle.addPlaceholder(BibtexOverviewEditor.ID);
-
-		IFolderLayout right = layout.createFolder("slr.right",
-				IPageLayout.RIGHT, 0.75f, editorArea);
-		right.addView(TaxonomyCheckboxListView.ID);
 	}
 }
