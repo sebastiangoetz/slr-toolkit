c9b4bf2d97fe5e83dc1c273dab3a7a7365cf3ed9
jasper-d <jasper-d@users.noreply.github.com>
2015-11-09 15:42:19 +0100

    Make perspective part of de.tudresden.slr.app and remove de.tudresden.slr.ui.perspective for now. Hide "Run" menu using a hack.
    #4, #28

--- plugins/de.tudresden.slr.app/src/de/tudresden/slr/app/ApplicationWorkbenchWindowAdvisor.java
+++ plugins/de.tudresden.slr.app/src/de/tudresden/slr/app/ApplicationWorkbenchWindowAdvisor.java
@@ -1,10 +1,14 @@
 package de.tudresden.slr.app;
 
+import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.eclipse.ui.internal.WorkbenchWindow;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
@@ -22,5 +26,25 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
         configurer.setShowCoolBar(false);
         configurer.setShowStatusLine(false);
         configurer.setTitle("SLR Toolkit"); //$NON-NLS-1$
+    }
+    
+    public void postWindowCreate(){
+    	hideRunMenu();
+    }
+    
+    //HACK: Only way to hide the "Run" menu that works ath the moment. WorkbenchWindow and getMenuBarManager() aren't API though. #28
+    private void hideRunMenu ()
+    {
+        IWorkbenchWindow workbenchWindow =  PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+        if(workbenchWindow instanceof WorkbenchWindow){
+            IContributionItem[] items = ((WorkbenchWindow) workbenchWindow).getMenuBarManager().getItems();
+            for (IContributionItem item : items)
+            {
+            	if(!item.getId().equalsIgnoreCase("org.eclipse.ui.run")){
+            		continue;
+            	}
+                item.setVisible(false);
+            }
+        }
     }
 }
