883502397499dee96142766763e34ab1b63bbfe0
jasper-d <jasper-d@users.noreply.github.com>
2015-11-23 18:22:29 +0100

    Minimal RCP app with product, a feature for dependencies and a minimal target definition.

---
+++ plugins/de.tudresden.slr.app/src/de/tudresden/slr/app/Perspective.java
@@ -0,0 +1,14 @@
+package de.tudresden.slr.app;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(true);
+		layout.setFixed(false);
+		
+	}
+
+}
