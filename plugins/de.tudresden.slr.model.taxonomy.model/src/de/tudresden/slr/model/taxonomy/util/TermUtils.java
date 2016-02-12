557213d4d285c3fdc47a4a89cefbc4c452446ec6
jasper-d <jasper-d@users.noreply.github.com>
2016-02-12 16:08:27 +0100

    Fixing term equality bug once again
    Also remove unused base class from TermComparer, make equals static and rename TermComparer to TermUtils

--- plugins/de.tudresden.slr.model.taxonomy.model/src/de/tudresden/slr/model/taxonomy/util/TermUtils.java
+++ plugins/de.tudresden.slr.model.taxonomy.model/src/de/tudresden/slr/model/taxonomy/util/TermUtils.java
@@ -1,19 +1,15 @@
 package de.tudresden.slr.model.taxonomy.util;
 
-import org.eclipse.emf.ecore.util.EcoreUtil.EqualityHelper;
-
 import de.tudresden.slr.model.taxonomy.Term;
 
-public class TermComparer extends EqualityHelper {
-	private static final long serialVersionUID = 4933778349620657774L;
-
+public class TermUtils {
 	/**
 	 * Compares two terms using their names (including their ancestors names) 
 	 * @param obj1 The first term
 	 * @param obj2 The second term
 	 * @return True if objects are equal, false otherwise
 	 */
-	public boolean equals(Term obj1, Term obj2){
+	public static boolean equals(Term obj1, Term obj2){
 		if(obj1 == null || obj2 == null || !obj1.getName().equals(obj2.getName())){
 			return false;
 		}
@@ -23,6 +19,6 @@ public class TermComparer extends EqualityHelper {
 		if(!(obj1.eContainer() instanceof Term) && !(obj2.eContainer() instanceof Term)){
 			return true;
 		}
-		return equals((Term)obj1.eContainer(), (Term)obj1.eContainer());
+		return equals((Term)obj1.eContainer(), (Term)obj2.eContainer());
 	}
 }
