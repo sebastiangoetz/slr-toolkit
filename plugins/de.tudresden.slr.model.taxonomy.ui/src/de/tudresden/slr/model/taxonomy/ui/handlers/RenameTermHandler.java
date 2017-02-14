e4b8a6de2e2930d2b88574beefe509f19f36e687
StephanPirnbaum <Stephan.Pirnbaum@googlemail.com>
2017-02-14 15:02:07 +0100

    #75 Enabled handler for renaming and changed inheritence to extending from the AbstractHandler

--- plugins/de.tudresden.slr.model.taxonomy.ui/src/de/tudresden/slr/model/taxonomy/ui/handlers/RenameTermHandler.java
+++ plugins/de.tudresden.slr.model.taxonomy.ui/src/de/tudresden/slr/model/taxonomy/ui/handlers/RenameTermHandler.java
@@ -1,46 +1,22 @@
 package de.tudresden.slr.model.taxonomy.ui.handlers;
 
+import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandler;
-import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
 
-public class RenameTermHandler implements IHandler {
-
-	@Override
-	public void addHandlerListener(IHandlerListener handlerListener) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
+public class RenameTermHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+		ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
+		if (selection == null || !(selection instanceof IStructuredSelection)) {
+			return null;
+		}
+		IStructuredSelection currentSelection = (IStructuredSelection) selection;
 		return null;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isHandled() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void removeHandlerListener(IHandlerListener handlerListener) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
