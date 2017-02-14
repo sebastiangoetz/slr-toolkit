3dd0df76887197adca813b20ffd0c1be4521d988
StephanPirnbaum <Stephan.Pirnbaum@googlemail.com>
2017-02-14 15:06:41 +0100

    #73 Created handler for term creation

---
+++ plugins/de.tudresden.slr.model.taxonomy.ui/src/de/tudresden/slr/model/taxonomy/ui/handlers/CreateTermHandler.java
@@ -0,0 +1,22 @@
+package de.tudresden.slr.model.taxonomy.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class CreateTermHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
+		if (selection == null || !(selection instanceof IStructuredSelection)) {
+			return null;
+		}
+		IStructuredSelection currentSelection = (IStructuredSelection) selection;
+		return null;
+	}
+
+}
