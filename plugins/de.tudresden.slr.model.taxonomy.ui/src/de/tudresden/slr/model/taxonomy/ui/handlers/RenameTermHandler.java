f52348ecefdf398ea461973ac9e02b670c0ddb50
StephanPirnbaum <stephan.pirnbaum@googlemail.com>
2017-02-21 16:41:53 +0100

    #79 Optimized bibtex export

--- plugins/de.tudresden.slr.model.taxonomy.ui/src/de/tudresden/slr/model/taxonomy/ui/handlers/RenameTermHandler.java
+++ plugins/de.tudresden.slr.model.taxonomy.ui/src/de/tudresden/slr/model/taxonomy/ui/handlers/RenameTermHandler.java
@@ -1,15 +1,20 @@
 package de.tudresden.slr.model.taxonomy.ui.handlers;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.ide.ResourceUtil;
 
 import de.tudresden.slr.model.bibtex.Document;
 import de.tudresden.slr.model.bibtex.util.BibtexFileWriter;
@@ -36,15 +41,24 @@ public class RenameTermHandler extends AbstractHandler {
 				Optional<Model> model = SearchUtils.getConainingModel(entry.getValue());
 				if (model.isPresent()) {
 					Model newTaxonomy = EcoreUtil.copy((Model) model.get());
-					entry.getKey().setTaxonomy(newTaxonomy);
-				}
-				BibtexFileWriter.updateBibtexFileForDocument(entry.getKey());
-				
-			}
+					entry.getKey().setTaxonomy(newTaxonomy);					
+				}				
+				BibtexFileWriter.updateBibtexFile(entry.getKey().eResource());
+			}			
 			Optional<Model> model = SearchUtils.getConainingModel(term);
 			if (model.isPresent()) {
 				Model newTaxonomy = EcoreUtil.copy((Model) model.get());
 				ModelRegistryPlugin.getModelRegistry().setActiveTaxonomy(newTaxonomy);
+				try {					
+					
+					Resource r = newTaxonomy.eResource();
+					IResource r1 = ResourceUtil.getResource(newTaxonomy);
+					r1.toString();
+					r.save(Collections.EMPTY_MAP);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}			
 		}		
 		return null;
