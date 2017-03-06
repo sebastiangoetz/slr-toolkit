e7ab78b8af89f83c9251ad3b326fb7f29d053340
StephanPirnbaum <stephan.pirnbaum@googlemail.com>
2017-03-06 12:41:34 +0100

    #73 Extracted logic

--- plugins/de.tudresden.slr.model.taxonomy.ui/src/de/tudresden/slr/model/taxonomy/ui/handlers/CreateTermHandler.java
+++ plugins/de.tudresden.slr.model.taxonomy.ui/src/de/tudresden/slr/model/taxonomy/ui/handlers/CreateTermHandler.java
@@ -1,23 +1,16 @@
 package de.tudresden.slr.model.taxonomy.ui.handlers;
 
-import java.util.Optional;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-import de.tudresden.slr.model.taxonomy.Model;
-import de.tudresden.slr.model.taxonomy.TaxonomyFactory;
 import de.tudresden.slr.model.taxonomy.Term;
 import de.tudresden.slr.model.taxonomy.ui.dialog.CreateTermDialog;
-import de.tudresden.slr.model.taxonomy.ui.dialog.CreateTermDialog.TermPosition;
-import de.tudresden.slr.model.utils.SearchUtils;
-import de.tudresden.slr.model.utils.TaxonomyUtils;
+import de.tudresden.slr.model.taxonomy.ui.manipulation.TermCreator;
 
 public class CreateTermHandler extends AbstractHandler {
 
@@ -33,23 +26,7 @@ public class CreateTermHandler extends AbstractHandler {
 			CreateTermDialog dialog = new CreateTermDialog(null, term.getName());
 			dialog.setBlockOnOpen(true);
 			if (dialog.open() == InputDialog.OK) {
-				// Some reflections to get factory
-				Term createdTerm = TaxonomyFactory.eINSTANCE.createTerm();
-				createdTerm.setName(dialog.getTermName());
-				if (dialog.getTermPosition() == TermPosition.SUBTERM) {
-					term.getSubclasses().add(createdTerm);
-				} else if (dialog.getTermPosition() == TermPosition.NEIGHBOR) {
-					EObject container = term.eContainer();
-					if (container instanceof Term) {
-						((Term) container).getSubclasses().add(createdTerm);
-					} else if (container instanceof Model) {
-						((Model) container).getDimensions().add(createdTerm);
-					}
-				}
-				Optional<Model> model = SearchUtils.getConainingModel(term);
-				if (model.isPresent()) {
-					TaxonomyUtils.saveTaxonomy(model.get());
-				}
+				TermCreator.create(dialog.getTermName(), term, dialog.getTermPosition());							
 			}
 		}	
 		return null;
---
+++ plugins/de.tudresden.slr.model.taxonomy.ui/src/de/tudresden/slr/model/taxonomy/ui/manipulation/TermCreator.java
@@ -0,0 +1,36 @@
+package de.tudresden.slr.model.taxonomy.ui.manipulation;
+
+import java.util.Optional;
+
+import org.eclipse.emf.ecore.EObject;
+
+import de.tudresden.slr.model.taxonomy.Model;
+import de.tudresden.slr.model.taxonomy.TaxonomyFactory;
+import de.tudresden.slr.model.taxonomy.Term;
+import de.tudresden.slr.model.taxonomy.ui.dialog.CreateTermDialog.TermPosition;
+import de.tudresden.slr.model.utils.SearchUtils;
+import de.tudresden.slr.model.utils.TaxonomyUtils;
+
+public class TermCreator {
+	
+	public static Term create(String name, Term relative, TermPosition position) {
+		Term createdTerm = TaxonomyFactory.eINSTANCE.createTerm();
+		createdTerm.setName(name);
+		if (position == TermPosition.SUBTERM) {
+			relative.getSubclasses().add(createdTerm);
+		} else if (position == TermPosition.NEIGHBOR) {
+			EObject container = relative.eContainer();
+			if (container instanceof Term) {
+				((Term) container).getSubclasses().add(createdTerm);
+			} else if (container instanceof Model) {
+				((Model) container).getDimensions().add(createdTerm);
+			}
+		}
+		Optional<Model> model = SearchUtils.getConainingModel(relative);
+		if (model.isPresent()) {
+			TaxonomyUtils.saveTaxonomy(model.get());
+		}
+		return createdTerm;
+	}
+
+}
