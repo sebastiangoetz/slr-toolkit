73f7746932fb0c58b79b28d21d2c66c8633cf35c
StephanPirnbaum <stephan.pirnbaum@googlemail.com>
2017-03-06 10:48:42 +0100

    #74 Extracted logic

--- plugins/de.tudresden.slr.model.taxonomy.ui/src/de/tudresden/slr/model/taxonomy/ui/handlers/DeleteTermHandler.java
+++ plugins/de.tudresden.slr.model.taxonomy.ui/src/de/tudresden/slr/model/taxonomy/ui/handlers/DeleteTermHandler.java
@@ -1,33 +1,19 @@
 package de.tudresden.slr.model.taxonomy.ui.handlers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-import de.tudresden.slr.model.bibtex.Document;
-import de.tudresden.slr.model.bibtex.util.BibtexFileWriter;
-import de.tudresden.slr.model.taxonomy.Model;
 import de.tudresden.slr.model.taxonomy.Term;
-import de.tudresden.slr.model.utils.SearchUtils;
-import de.tudresden.slr.model.utils.TaxonomyUtils;
+import de.tudresden.slr.model.taxonomy.ui.manipulation.TermDeleter;
 
 public class DeleteTermHandler extends AbstractHandler {
 
@@ -54,36 +40,14 @@ public class DeleteTermHandler extends AbstractHandler {
 			dialog.setBlockOnOpen(true);
 			if (dialog.open() == MessageDialog.OK && dialog.getReturnCode() == MessageDialog.OK) {
 				List<Term> termsToDelete = new ArrayList<>(currentSelection.size());
-				currentSelection.toList().stream().filter(s -> s instanceof Term).forEach(s -> termsToDelete.add((Term) s));;
-				deleteTerm(termsToDelete);
+				currentSelection.toList().stream()
+						.filter(s -> s instanceof Term)
+						.forEach(s -> termsToDelete.add((Term) s));;
+				TermDeleter.delete(termsToDelete);
 			}
 		}
 		
 		return null;
-	}
-	
-	private void deleteTerm(List<Term> termsToDelete) {
-		Map<Document, List<Term>> termsInDocuments = new HashMap<>(); 
-		for (Term term : termsToDelete) {
-			Map<Document, Term> termInDocuments = SearchUtils.findDocumentsWithTerm(term);
-			termInDocuments.forEach((k, v) -> {
-					if (termsInDocuments.containsKey(k)) termsInDocuments.get(k).add(v);
-					else termsInDocuments.put(k, new LinkedList<Term>(Arrays.asList(v)));
-			});  
-			Optional<Model> model = SearchUtils.getConainingModel(term);
-			EcoreUtil.remove(term);			
-			if (model.isPresent()) {
-				TaxonomyUtils.saveTaxonomy(model.get());
-			}
-		}
-		Set<Resource> resourcesToUpdate = new TreeSet<>(
-				(Resource r1, Resource r2) -> r1 == r2 ? 0 : 1);
-		for (Map.Entry<Document, List<Term>> entry : termsInDocuments.entrySet()) {	
-			entry.getValue().forEach(t -> EcoreUtil.remove(t));
-			Model newTaxonomy = EcoreUtil.copy(entry.getKey().getTaxonomy());
-			resourcesToUpdate.add(entry.getKey().eResource());
-		}
-		resourcesToUpdate.forEach(r -> BibtexFileWriter.updateBibtexFile(r));
 	}
 
 }
