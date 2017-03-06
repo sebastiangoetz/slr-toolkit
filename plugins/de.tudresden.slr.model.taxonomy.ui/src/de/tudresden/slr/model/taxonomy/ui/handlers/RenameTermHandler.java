8b066288a516e3d489179a45f9ba4aa74c4b1bb9
StephanPirnbaum <stephan.pirnbaum@googlemail.com>
2017-03-06 10:34:14 +0100

    #75 Extracted logic

--- plugins/de.tudresden.slr.model.taxonomy.ui/src/de/tudresden/slr/model/taxonomy/ui/handlers/RenameTermHandler.java
+++ plugins/de.tudresden.slr.model.taxonomy.ui/src/de/tudresden/slr/model/taxonomy/ui/handlers/RenameTermHandler.java
@@ -1,26 +1,15 @@
 package de.tudresden.slr.model.taxonomy.ui.handlers;
 
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.TreeSet;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-import de.tudresden.slr.model.bibtex.Document;
-import de.tudresden.slr.model.bibtex.util.BibtexFileWriter;
-import de.tudresden.slr.model.taxonomy.Model;
 import de.tudresden.slr.model.taxonomy.Term;
-import de.tudresden.slr.model.utils.SearchUtils;
-import de.tudresden.slr.model.utils.TaxonomyUtils;
+import de.tudresden.slr.model.taxonomy.ui.manipulation.TermRenamer;
 
 public class RenameTermHandler extends AbstractHandler {
 
@@ -40,31 +29,10 @@ public class RenameTermHandler extends AbstractHandler {
 					null);
 			dialog.setBlockOnOpen(true);
 			if (dialog.open() == InputDialog.OK) {
-				renameTerm(term, dialog.getValue());
+				TermRenamer.rename(term, dialog.getValue());
 			}
 		}		
 		return null;
-	}
-
-	private void renameTerm(Term term, String newValue) {
-		Map<Document, Term> termsInDocuments = SearchUtils.findDocumentsWithTerm(term);
-		term.setName(newValue);
-		Set<Resource> resourcesToUpdate = new TreeSet<>(
-				(Resource r1, Resource r2) -> r1 == r2 ? 0 : 1);
-		for (Map.Entry<Document, Term> entry : termsInDocuments.entrySet()) {
-			entry.getValue().setName(newValue);
-			Optional<Model> model = SearchUtils.getConainingModel(entry.getValue());
-			if (model.isPresent()) {
-				Model newTaxonomy = EcoreUtil.copy((Model) model.get());
-				entry.getKey().setTaxonomy(newTaxonomy);					
-			}				
-			resourcesToUpdate.add(entry.getKey().eResource());			
-		}			
-		resourcesToUpdate.forEach(r -> BibtexFileWriter.updateBibtexFile(r));		
-		Optional<Model> model = SearchUtils.getConainingModel(term);
-		if (model.isPresent()) {
-			TaxonomyUtils.saveTaxonomy(model.get());			
-		}
 	}
 
 }
---
+++ plugins/de.tudresden.slr.model.taxonomy.ui/src/de/tudresden/slr/model/taxonomy/ui/manipulation/TermRenamer.java
@@ -0,0 +1,41 @@
+package de.tudresden.slr.model.taxonomy.ui.manipulation;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
+import de.tudresden.slr.model.bibtex.Document;
+import de.tudresden.slr.model.bibtex.util.BibtexFileWriter;
+import de.tudresden.slr.model.taxonomy.Model;
+import de.tudresden.slr.model.taxonomy.Term;
+import de.tudresden.slr.model.utils.SearchUtils;
+import de.tudresden.slr.model.utils.TaxonomyUtils;
+
+public class TermRenamer {
+
+	public static Term rename(Term term, String name) {
+		Map<Document, Term> termsInDocuments = SearchUtils.findDocumentsWithTerm(term);
+		term.setName(name);
+		Set<Resource> resourcesToUpdate = new TreeSet<>(
+				(Resource r1, Resource r2) -> r1 == r2 ? 0 : 1);
+		for (Map.Entry<Document, Term> entry : termsInDocuments.entrySet()) {
+			entry.getValue().setName(name);
+			Optional<Model> model = SearchUtils.getConainingModel(entry.getValue());
+			if (model.isPresent()) {
+				Model newTaxonomy = EcoreUtil.copy((Model) model.get());
+				entry.getKey().setTaxonomy(newTaxonomy);					
+			}				
+			resourcesToUpdate.add(entry.getKey().eResource());			
+		}			
+		resourcesToUpdate.forEach(r -> BibtexFileWriter.updateBibtexFile(r));		
+		Optional<Model> model = SearchUtils.getConainingModel(term);
+		if (model.isPresent()) {
+			TaxonomyUtils.saveTaxonomy(model.get());			
+		}
+		return term;
+	}
+}
