bcfb453f6c8a1d09b96e28a98644010db15b1efb
StephanPirnbaum <stephan.pirnbaum@googlemail.com>
2017-03-14 13:12:32 +0100

    #82 Activated formatter for taxonomy export

--- plugins/de.tudresden.slr.model.modelregistry/src/de/tudresden/slr/model/utils/TaxonomyUtils.java
+++ plugins/de.tudresden.slr.model.modelregistry/src/de/tudresden/slr/model/utils/TaxonomyUtils.java
@@ -1,9 +1,9 @@
 package de.tudresden.slr.model.utils;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.xtext.resource.SaveOptions;
 
 import de.tudresden.slr.model.modelregistry.ModelRegistryPlugin;
 import de.tudresden.slr.model.taxonomy.Model;
@@ -15,7 +15,9 @@ public class TaxonomyUtils {
 		ModelRegistryPlugin.getModelRegistry().setActiveTaxonomy(newTaxonomy);
 		try {					
 			if (newTaxonomy.getResource() == null) newTaxonomy.setResource(taxonomy.eResource());
-			newTaxonomy.getResource().save(Collections.EMPTY_MAP);
+			SaveOptions.Builder optionsBuilder = SaveOptions.newBuilder();
+			optionsBuilder.format();
+			newTaxonomy.getResource().save(optionsBuilder.getOptions().toOptionsMap());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
