6e896981b7fad71d34a6aa01f784d2be1f3302e2
Unknown <richard.kielmann@gmail.com>
2018-01-19 12:25:32 +0100

    wip

--- plugins/de.tudresden.slr.ui.chart/src/de/tudresden/slr/ui/chart/logic/BubbleChartGenerator.java
+++ plugins/de.tudresden.slr.ui.chart/src/de/tudresden/slr/ui/chart/logic/BubbleChartGenerator.java
@@ -1,6 +1,9 @@
 package de.tudresden.slr.ui.chart.logic;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.birt.chart.extension.datafeed.BubbleEntry;
 import org.eclipse.birt.chart.model.Chart;
@@ -20,10 +23,12 @@ import org.eclipse.birt.chart.model.data.NumberDataSet;
 import org.eclipse.birt.chart.model.data.OrthogonalSampleData;
 import org.eclipse.birt.chart.model.data.SampleData;
 import org.eclipse.birt.chart.model.data.SeriesDefinition;
+import org.eclipse.birt.chart.model.data.TextDataSet;
 import org.eclipse.birt.chart.model.data.impl.BubbleDataSetImpl;
 import org.eclipse.birt.chart.model.data.impl.NumberDataElementImpl;
 import org.eclipse.birt.chart.model.data.impl.NumberDataSetImpl;
 import org.eclipse.birt.chart.model.data.impl.SeriesDefinitionImpl;
+import org.eclipse.birt.chart.model.data.impl.TextDataSetImpl;
 import org.eclipse.birt.chart.model.impl.ChartWithAxesImpl;
 import org.eclipse.birt.chart.model.layout.Legend;
 import org.eclipse.birt.chart.model.layout.Plot;
@@ -64,12 +69,11 @@ public class BubbleChartGenerator
 
 		// X-Axis
 		Axis xAxisPrimary = cwaBubble.getPrimaryBaseAxes( )[0];
-		xAxisPrimary.setType( AxisType.LINEAR_LITERAL );
+		xAxisPrimary.setType( AxisType.TEXT_LITERAL );
 		xAxisPrimary.getMajorGrid( ).setTickStyle( TickStyle.BELOW_LITERAL );
 		xAxisPrimary.getMajorGrid().getLineAttributes().setVisible(true);
 		xAxisPrimary.getOrigin( ).setType( IntersectionType.MIN_LITERAL );
-		xAxisPrimary.getScale( ).setMin( NumberDataElementImpl.create( 0 ) );
-		xAxisPrimary.getScale( ).setMax( NumberDataElementImpl.create( 140 ) );
+		
 		
 		// Y-Axis
 		Axis yAxisPrimary = cwaBubble.getPrimaryOrthogonalAxis( xAxisPrimary );
@@ -84,30 +88,48 @@ public class BubbleChartGenerator
 		NumberDataSet categoryValues = NumberDataSetImpl.create( new double[] {
 				20,45,70,100,120,130
 		});
+		Set<String> xValues = new HashSet<String>();
+		for (BubbleDataContainer data : input) {
+			xValues.add(data.getxTerm().toString());
+		}
+		String[] xEntries = new String[xValues.size()];
+		xEntries = xValues.toArray(xEntries);
+		TextDataSet xAxisValues = TextDataSetImpl.create(xEntries);
+		
+		/*for(String inp : xValues) {
+			System.out.println(inp.toString());
+		}*/
+		
+		
+		
+		
+		
+		//System.out.println(xAxisValues.toString());
+		/*System.out.println(input.toString());
+		for(BubbleDataContainer data: input) {
+			System.out.println(data.getxTerm().toString());
+			System.out.println(data.getyTerm().toString());
+			System.out.println(data.getBubbleSize());
+		}*/
+		List<BubbleDataSet> bubbleEntries = new ArrayList<BubbleDataSet>();
+		for(BubbleDataContainer data : input) {
+			bubbleEntries.add(BubbleDataSetImpl.create(new BubbleEntry(data.getxTerm(), data.getBubbleSize())));
+		}
+		
+		/*for(BubbleDataSet series : bubbleEntries) {
+			System.out.println(series.getValues().toString());
+		}*/
+
+		
 		BubbleDataSet values1 = BubbleDataSetImpl.create( new BubbleEntry[]{
 				null,
-				new BubbleEntry( 15, 100 ),
-				new BubbleEntry( Integer.valueOf( 18 ), Integer.valueOf( 80 ) ),
+				new BubbleEntry( "architecture", 100 ),
+				new BubbleEntry( "none", Integer.valueOf( 80 ) ),
 				null,
-				new BubbleEntry( Integer.valueOf( 23 ), Integer.valueOf( 100 ) ),
+				new BubbleEntry( "component", Integer.valueOf( 100 ) ),
 				null
 		} );
-		BubbleDataSet values2 = BubbleDataSetImpl.create( new BubbleEntry[]{
-				new BubbleEntry( Integer.valueOf( 50 ), Integer.valueOf( 60 ) ),
-				null,
-				null,
-				new BubbleEntry( Integer.valueOf( 43 ), Integer.valueOf( 80 ) ),
-				new BubbleEntry( Integer.valueOf( 12 ), Integer.valueOf( 100 ) ),
-				null
-		} );
-		BubbleDataSet values3 = BubbleDataSetImpl.create( new BubbleEntry[]{
-				null,
-				null,
-				new BubbleEntry( Integer.valueOf( 43 ), Integer.valueOf( 75 ) ),
-				new BubbleEntry( Integer.valueOf( 31 ), Integer.valueOf( 93 ) ),
-				null,
-				new BubbleEntry( Integer.valueOf( 25 ), Integer.valueOf( 50 ) )
-		} );
+		
 		SampleData sd = DataFactory.eINSTANCE.createSampleData( );
 		BaseSampleData sdBase = DataFactory.eINSTANCE.createBaseSampleData( );
 		sdBase.setDataSetRepresentation( "" );//$NON-NLS-1$
@@ -122,7 +144,7 @@ public class BubbleChartGenerator
 
 		// X-Series
 		Series seCategory = SeriesImpl.create( );
-		seCategory.setDataSet( categoryValues );
+		seCategory.setDataSet( xAxisValues );
 
 		SeriesDefinition sdX = SeriesDefinitionImpl.create( );
 		sdX.getSeriesPalette( ).shift( 0 );
@@ -133,18 +155,7 @@ public class BubbleChartGenerator
 		BubbleSeries bs1 = (BubbleSeries) BubbleSeriesImpl.create( );
 		bs1.setDataSet( values1 );
 		bs1.getLabel( ).setVisible( false );
-		
-		BubbleSeries bs2 = (BubbleSeries) BubbleSeriesImpl.create( );
-		bs2.setDataSet( values2 );
-		bs2.getLabel( ).setVisible( false );
-		
-		
-		BubbleSeries bs3 = (BubbleSeries) BubbleSeriesImpl.create( );
-		bs3.setDataSet( values3 );
-		
-		bs3.getLabel( ).setVisible( true );
-		System.out.println(bs3.getDisplayName());
-
+	
 		
 
 		SeriesDefinition sdY = SeriesDefinitionImpl.create( );
@@ -153,8 +164,6 @@ public class BubbleChartGenerator
 
 		yAxisPrimary.getSeriesDefinitions( ).add( sdY );
 		sdY.getSeries( ).add( bs1 );
-		sdY.getSeries( ).add( bs2 );
-		sdY.getSeries( ).add( bs3 );
 		sdY.getSeriesPalette().getEntries().clear();
 		sdY.getSeriesPalette().getEntries().add(ColorDefinitionImpl.BLACK());
 		
