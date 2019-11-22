28f5de477bbac70a8885e60917da94ede4076a8f
Martin Volkmann <martin.volkmann96@gmail.com>
2019-11-22 10:57:18 +0100

    basic css support

--- plugins/de.tudresden.slr.questionnaire/src/de/tudresden/slr/questionnaire/HtmlSummary.java
+++ plugins/de.tudresden.slr.questionnaire/src/de/tudresden/slr/questionnaire/HtmlSummary.java
@@ -4,7 +4,15 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
 
 import de.tudresden.slr.questionnaire.model.ChoiceQuestion;
 import de.tudresden.slr.questionnaire.model.FreeTextQuestion;
@@ -21,16 +29,16 @@ public class HtmlSummary {
 		super();
 		this.questionnaire = questionnaire;
 	}
-	
+
 	public void showSummary() {
 		try {
 			File file = File.createTempFile("questionnaire-summary-", ".html");
 			file.deleteOnExit();
-			
+
 			PrintWriter pw = new PrintWriter(file);
 			pw.write(generateSummary());
 			pw.close();
-			
+
 			Desktop.getDesktop().browse(file.toURI());
 		} catch (IOException e) {
 			throw new RuntimeException(e);
@@ -39,23 +47,31 @@ public class HtmlSummary {
 
 	public String generateSummary() {
 		StringBuilder str = new StringBuilder();
-		str.append("<html><body>");
+		str.append("<html>");
+		
+		str.append("<head>");
+		str.append("<title>Questionnaire Summary</title>");
+		addCssFile("css/reset.css", str);
+		addCssFile("css/summary.css", str);
+		str.append("</head>");
+		
+		str.append("<body>");
 		questionnaire.getQuestions().forEach(q -> visit(str, q));
-		str.append("</body></html>");
+		str.append("</body>");
+		
+		str.append("</html>");
 		return str.toString();
 	}
 
 	private void visit(StringBuilder stringBuilder, Question<?> question) {
-		stringBuilder.append("<h1>").append(question.getQuestionText())
-				.append("</h1>");
+		stringBuilder.append("<h1>").append(question.getQuestionText()).append("</h1>");
 
 		if (question instanceof FreeTextQuestion)
 			visit(stringBuilder, (FreeTextQuestion) question);
 		else if (question instanceof ChoiceQuestion)
 			visit(stringBuilder, (ChoiceQuestion<?>) question);
 		else
-			throw new IllegalArgumentException(
-					"not implemented for " + question.getClass());
+			throw new IllegalArgumentException("not implemented for " + question.getClass());
 	}
 
 	private void visit(StringBuilder str, FreeTextQuestion question) {
@@ -73,11 +89,31 @@ public class HtmlSummary {
 		for (String choice : question.getChoices()) {
 			str.append("<tr>");
 			str.append("<td>").append(choice).append("</td>");
-			str.append("<td>")
-					.append(question.countOccurrencesOfChoiceInAnswers(choice))
-					.append("</td>");
+			str.append("<td>").append(question.countOccurrencesOfChoiceInAnswers(choice)).append("</td>");
 			str.append("</tr>");
 		}
 		str.append("</table>");
+	}
+	
+	private void addCssFile(String cssName, StringBuilder str) {
+		Bundle bundle = Platform.getBundle("de.tudresden.slr.questionnaire");
+		URL url = bundle.getEntry(cssName);
+		System.out.println(bundle);
+		System.out.println(url);
+		try {
+			File file = new File(FileLocator.resolve(url).toURI());
+			addCssFile(file, str);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void addCssFile(File file, StringBuilder str) {
+		try {
+			String content = Files.lines(file.toPath()).collect(Collectors.joining());
+			str.append("<style>").append(content).append("</style>");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
