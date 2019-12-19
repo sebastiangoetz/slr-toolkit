38be7687a8ca5d5467d379df292c188a317d1161
Martin Volkmann <martin.volkmann96@gmail.com>
2019-12-19 10:47:45 +0100

    change text of UnansweredDocumentsView depending on how many documents are missing

--- plugins/de.tudresden.slr.questionnaire/src/de/tudresden/slr/questionnaire/UnansweredDocumentsView.java
+++ plugins/de.tudresden.slr.questionnaire/src/de/tudresden/slr/questionnaire/UnansweredDocumentsView.java
@@ -20,63 +20,72 @@ import de.tudresden.slr.questionnaire.model.Question;
 import de.tudresden.slr.questionnaire.util.BibtexExplorer;
 
 public class UnansweredDocumentsView {
-	private Label incompleteDocumentsLabel;
-	private ListViewer incompleteDocumentsList;
+    private Label incompleteDocumentsLabel;
+    private ListViewer incompleteDocumentsList;
 
-	private Supplier<Questionnaire> questionnaireSupplier;
-	private Supplier<IProject> projectSupplier;
+    private Supplier<Questionnaire> questionnaireSupplier;
+    private Supplier<IProject> projectSupplier;
 
-	private List<Document> incompleteDocuments = new LinkedList<>();
+    private List<Document> incompleteDocuments = new LinkedList<>();
 
-	public UnansweredDocumentsView(Composite parent, IWorkbenchPartSite site,
-			Supplier<Questionnaire> questionnaireSupplier, Supplier<IProject> projectSupplier) {
-		this.questionnaireSupplier = questionnaireSupplier;
-		this.projectSupplier = projectSupplier;
+    private static String TEMPLATE_ANSWERS_MISSING = "Questionnaire is missing answers for %d documents.";
+    private static String TEMPLATE_ANSWERS_COMPLETE = "Questionnaire is fully answered.";
 
-		incompleteDocumentsLabel = new Label(parent, 0);
-		incompleteDocumentsLabel.setText("Answers missing for following documents:");
-		incompleteDocumentsLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.BEGINNING, false, false, 2, 1));
+    public UnansweredDocumentsView(Composite parent, IWorkbenchPartSite site,
+            Supplier<Questionnaire> questionnaireSupplier, Supplier<IProject> projectSupplier) {
+        this.questionnaireSupplier = questionnaireSupplier;
+        this.projectSupplier = projectSupplier;
 
-		incompleteDocumentsList = new ListViewer(parent, SWT.V_SCROLL);
-		GridData gd = new GridData(SWT.BEGINNING, SWT.BEGINNING, false, false, 2, 1);
-		gd.heightHint = (int) (((org.eclipse.swt.widgets.List) incompleteDocumentsList.getControl()).getItemHeight()
-				* 4);
-		gd.widthHint = 300;
-		incompleteDocumentsList.getControl()
-				.setLayoutData(gd);
-		incompleteDocumentsList.setSorter(new ViewerSorter());
-		incompleteDocumentsList.setContentProvider(ArrayContentProvider.getInstance());
-		incompleteDocumentsList.setLabelProvider(new LabelProvider() {
-			@Override
-			public String getText(Object element) {
-				return ((Document) element).getKey();
-			}
-		});
-		site.setSelectionProvider(incompleteDocumentsList);
-	}
+        incompleteDocumentsLabel = new Label(parent, 0);
+        incompleteDocumentsLabel.setText(TEMPLATE_ANSWERS_COMPLETE);
+        incompleteDocumentsLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.BEGINNING, false, false, 2, 1));
 
-	public List<Document> findDocuments() {
-		List<Document> badDocuments = new LinkedList<>();
-		IProject project = projectSupplier.get();
-		Questionnaire questionnaire = questionnaireSupplier.get();
-		if (questionnaire != null && project != null) {
-			BibtexExplorer be = new BibtexExplorer(project);
-			List<Question<?>> questions = questionnaire.getQuestions();
-			List<Document> documents = be.getDocuments();
-			for (Document doc : documents) {
-				for (Question<?> question : questions) {
-					if (!question.hasAnswerFor(doc.getKey())) {
-						badDocuments.add(doc);
-						break;
-					}
-				}
-			}
-		}
-		if (!badDocuments.equals(incompleteDocuments)) {
-			incompleteDocumentsList.setInput(badDocuments);
-			incompleteDocuments = badDocuments;
-		}
-		return badDocuments;
-	}
+        incompleteDocumentsList = new ListViewer(parent, SWT.V_SCROLL);
+        GridData gd = new GridData(SWT.BEGINNING, SWT.BEGINNING, false, false, 2, 1);
+        gd.heightHint = (int) (((org.eclipse.swt.widgets.List) incompleteDocumentsList.getControl()).getItemHeight()
+                * 4);
+        gd.widthHint = 300;
+        incompleteDocumentsList.getControl().setLayoutData(gd);
+        incompleteDocumentsList.getControl().setVisible(false);
+        incompleteDocumentsList.setSorter(new ViewerSorter());
+        incompleteDocumentsList.setContentProvider(ArrayContentProvider.getInstance());
+        incompleteDocumentsList.setLabelProvider(new LabelProvider() {
+            @Override
+            public String getText(Object element) {
+                return ((Document) element).getKey();
+            }
+        });
+        site.setSelectionProvider(incompleteDocumentsList);
+    }
+
+    public List<Document> findDocuments() {
+        List<Document> badDocuments = new LinkedList<>();
+        IProject project = projectSupplier.get();
+        Questionnaire questionnaire = questionnaireSupplier.get();
+        if (questionnaire != null && project != null) {
+            BibtexExplorer be = new BibtexExplorer(project);
+            List<Question<?>> questions = questionnaire.getQuestions();
+            List<Document> documents = be.getDocuments();
+            for (Document doc : documents) {
+                for (Question<?> question : questions) {
+                    if (!question.hasAnswerFor(doc.getKey())) {
+                        badDocuments.add(doc);
+                        break;
+                    }
+                }
+            }
+        }
+        if (!badDocuments.equals(incompleteDocuments)) {
+            incompleteDocumentsList.setInput(badDocuments);
+            incompleteDocuments = badDocuments;
+
+            String newDisplayText = TEMPLATE_ANSWERS_COMPLETE;
+            if (!incompleteDocuments.isEmpty())
+                newDisplayText = String.format(TEMPLATE_ANSWERS_MISSING, incompleteDocuments.size());
+            incompleteDocumentsLabel.setText(newDisplayText);
+            incompleteDocumentsList.getControl().setVisible(!incompleteDocuments.isEmpty());
+        }
+        return badDocuments;
+    }
 
 }
