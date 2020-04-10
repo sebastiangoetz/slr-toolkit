93e8958c3f12806e81b8514446414d31722bed91
BackCatalogSmog <uli.hubi@onlinehome.de>
2020-04-10 18:21:08 +0200

    Added method documentation to StandardTaxonomyClassifier
    Added javadoc annotations to StandardTaxonomyClassifier

--- plugins/de.tudresden.slr.classification/src/de/tudresden/slr/classification/classifiers/StandardTaxonomyClassifier.java
+++ plugins/de.tudresden.slr.classification/src/de/tudresden/slr/classification/classifiers/StandardTaxonomyClassifier.java
@@ -33,118 +33,161 @@ import de.tudresden.slr.utils.taxonomy.manipulation.TermCreator;
 
 public class StandardTaxonomyClassifier {
 
-	
-	
 	public StandardTaxonomyClassifier() {
-		
+
 	}
-	
+
+	/**
+	 * Alias for createStandardTaxonomy(d, null)
+	 * 
+	 * @param d
+	 *            Document to classify
+	 */
+	public void createStandardTaxonomy(Document d) {
+		createStandardTaxonomy(d, null);
+	}
+
+	/**
+	 * Creates a taxonomy for the given document based on bibtex attributes
+	 * (Document type and Document Venue). If provided, it will use a cross
+	 * reference to avoid inconsistent journal/book names If SCOPUS document types
+	 * are found, those specified in that field will be used instead of annoted
+	 * types Term names are stripped of non-alphanumeric characters and malformed
+	 * term names will be corrected by handleMalformedTermName
+	 * 
+	 * @param doc
+	 *            Document to be classified
+	 * @param crossRefTitle
+	 *            Title of the document to be cross referenced If null, no cross
+	 *            reference will be used
+	 */
 	public void createStandardTaxonomy(Document doc, String crossRefTitle) {
-		Map<String,String> fields = doc.getAdditionalFields();
+		Map<String, String> fields = doc.getAdditionalFields();
 		Set<String> fieldKeys = fields.keySet();
-		if(fieldKeys.contains("document_type")) {
-			classifyDocument(doc,"Document Type", fields.get("document_type"));
+		if (fieldKeys.contains("document_type")) {
+			classifyDocument(doc, "Document Type", fields.get("document_type"));
 		} else {
-			classifyDocument(doc,"Document Type", doc.getType());
+			classifyDocument(doc, "Document Type", doc.getType());
 		}
-		
-		String[] lookUp = {"booktitle","journal","howpublished"};
+
+		String[] lookUp = { "booktitle", "journal", "howpublished" };
 		String venueType = "";
 		boolean venueFound = false;
-		for(String s: lookUp) {
-			if(fields.containsKey(s)) {
+		for (String s : lookUp) {
+			if (fields.containsKey(s)) {
 				venueType = s;
 				venueFound = true;
 				break;
 			}
-			
+
 		}
-		if(venueFound) {
+		if (venueFound) {
 			String field = "";
-			if(crossRefTitle == null) {
+			if (crossRefTitle == null) {
 				field = fields.get(venueType).replaceAll("[^A-Za-z0-9 ]", "");
 			} else {
 				field = crossRefTitle.replaceAll("[^A-Za-z0-9 ]", "");
 			}
-			
-			if(isTermNameMalformed(field)) {
-				field = handleMalformedTermName(field,false);
+
+			if (isTermNameMalformed(field)) {
+				field = handleMalformedTermName(field, false);
 			}
-			
-			classifyDocument(doc,"Document Venue", venueType, field);
+
+			classifyDocument(doc, "Document Venue", venueType, field);
 		}
-		
-	 }
-	
+
+	}
+
+	/**
+	 * Checks if a term name is malformed and cannot be used
+	 * 
+	 * @param name
+	 *            Name to be checked
+	 * @return malformedness
+	 */
 	private boolean isTermNameMalformed(String name) {
 		return !Character.isLetter(name.charAt(0)) || !name.matches("[A-Za-z0-9 ]+");
 	}
-	
-	private String handleMalformedTermName(String name,boolean useDialog) {
-		
+
+	/**
+	 * Transforms strings that would be invalid when used as term names into valid
+	 * ones
+	 * 
+	 * @param name
+	 *            Term name to be transformed
+	 * @param useDialog
+	 *            Indicates, wether a pop-up string field that allows users to set
+	 *            the term name manually should appear
+	 * @return Valid term name
+	 */
+	private String handleMalformedTermName(String name, boolean useDialog) {
+
 		String returnName = "T " + name;
-		if(useDialog) {
+		if (useDialog) {
 			InputDialog dlg = new InputDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-					 "Invalid Term name",
-					 "Please enter a new term name that starts with a letter.",
-					 returnName,
-					 (s -> (!Character.isLetter(s.charAt(0))) ? "" : null)
-							 );
-			 returnName = (dlg.open() == 0)? name = dlg.getValue() : returnName;
-			 returnName.replaceAll("[^A-Za-z0-9 ]", "");
+					"Invalid Term name", "Please enter a new term name that starts with a letter.", returnName,
+					(s -> (!Character.isLetter(s.charAt(0))) ? "" : null));
+			returnName = (dlg.open() == 0) ? name = dlg.getValue() : returnName;
+			returnName.replaceAll("[^A-Za-z0-9 ]", "");
 		}
 		return returnName;
 	}
-	
-	public void createStandardTaxonomy(Document d) {
-		createStandardTaxonomy(d,null);
-	}
-	 
-	 public void classifyDocument(Document doc, String ...types) {
-		 Optional<Model> modelOptional =  ModelRegistryPlugin.getModelRegistry().getActiveTaxonomy();
-		 if(modelOptional.isPresent()) {
-			 Model model = modelOptional.get();
-			 
-			 EObject taxonomyElement = model;
-			 EObject docTaxonomyElement = doc.getTaxonomy();
-			 
-			for(String type: types) {
-				 //create element in general taxonomy 
-				 taxonomyElement = TermCreator.createChildIfNotExisting(taxonomyElement,type);
-				 docTaxonomyElement = TermCreator.createChildIfNotExisting(docTaxonomyElement,type,false);
-			 }
-			 
-			 TaxonomyUtils.saveTaxonomy(model);
-			 try {
+
+	/**
+	 * Adds hierarchical terms to a document based on the order of supplied strings
+	 * 
+	 * @param doc  document for which to create terms
+	 * @param types  variable amount of strings starting with the highest order term name
+	 */
+	public void classifyDocument(Document doc, String... types) {
+		Optional<Model> modelOptional = ModelRegistryPlugin.getModelRegistry().getActiveTaxonomy();
+		if (modelOptional.isPresent()) {
+			Model model = modelOptional.get();
+
+			EObject taxonomyElement = model;
+			EObject docTaxonomyElement = doc.getTaxonomy();
+
+			for (String type : types) {
+				// create element in general taxonomy
+				taxonomyElement = TermCreator.createChildIfNotExisting(taxonomyElement, type);
+				docTaxonomyElement = TermCreator.createChildIfNotExisting(docTaxonomyElement, type, false);
+			}
+
+			TaxonomyUtils.saveTaxonomy(model);
+			try {
 				doc.eResource().save(null);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		 }
-	 }
-	 
-	 public void classifyDocumentsInProject(IProject project) {
-		 try {
-			 List<Document> docs = ModelRegistryPlugin.getModelRegistry().getResourceManager().loadProject(project);
-			 Map<String,String> docMap = new HashMap<>();
-			 
-				for(Document d: docs) {
-					docMap.put(d.getKey(), d.getTitle());	
+		}
+	}
+
+	/**
+	 * Loads project if necessary and generates a taxonomy based on bibtex and taxonomy files
+	 * 
+	 * @param project  Project containing bibtex and taxonomy files
+	 */
+	public void classifyDocumentsInProject(IProject project) {
+		try {
+			List<Document> docs = ModelRegistryPlugin.getModelRegistry().getResourceManager().loadProject(project);
+			Map<String, String> docMap = new HashMap<>();
+
+			for (Document d : docs) {
+				docMap.put(d.getKey(), d.getTitle());
+			}
+
+			for (Document doc : docs) {
+				if (doc.getAdditionalFields().containsKey("crossref")) {
+					createStandardTaxonomy(doc, docMap.get(doc.getAdditionalFields().get("crossref")));
+				} else {
+					createStandardTaxonomy(doc);
 				}
-				
-				for(Document doc: docs) {
-					if(doc.getAdditionalFields().containsKey("crossref")) {
-						createStandardTaxonomy(doc,docMap.get(doc.getAdditionalFields().get("crossref")));
-					} else {
-						createStandardTaxonomy(doc);
-					}
-					
-				}
-		 } catch (CoreException e) {
-			 
-		 }
-		 
-		 
-	 }
+
+			}
+		} catch (CoreException e) {
+
+		}
+
+	}
 
 }
