242c213506500e439f35404f34225475bf74630f
David Tiede <tidenhub@gmx.de>
2020-12-08 17:25:11 +0100

    Fix Repo entity

--- android-client/app/src/main/java/de/davidtiede/slrtoolkit/database/Repo.java
+++ android-client/app/src/main/java/de/davidtiede/slrtoolkit/database/Repo.java
@@ -3,13 +3,14 @@ package de.davidtiede.slrtoolkit.database;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-import java.nio.file.Path;
-
 @Entity
 public class Repo {
     @PrimaryKey
     public int id;
-
     public String name;
-    public Path path;
+    public String local_path;
+    public String remote_url;
+    public String username;
+    public String git_name;
+    public String git_email;
 }
