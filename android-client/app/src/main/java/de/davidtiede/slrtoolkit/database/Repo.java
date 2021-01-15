aa3867c76729c1634f42f80cc6af3654e11e4d29
David Tiede <tidenhub@gmx.de>
2021-01-15 15:28:46 +0100

    Repo add getter & setter

--- android-client/app/src/main/java/de/davidtiede/slrtoolkit/database/Repo.java
+++ android-client/app/src/main/java/de/davidtiede/slrtoolkit/database/Repo.java
@@ -1,26 +1,69 @@
 package de.davidtiede.slrtoolkit.database;
 
-import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity
 public class Repo {
-    @PrimaryKey
-    public int id;
-    public String name;
-    public String local_path;
-    public String remote_url;
-    public String username;
-    public String git_name;
-    public String git_email;
+    @PrimaryKey(autoGenerate = true) private int id;
+    private String name;
+    private String local_path;
+    private String remote_url;
+    private String username;
+    private String token;
+    private String git_name;
+    private String git_email;
 
-    public Repo(String remote_url) {
-        this.name = remote_url;
+    public Repo(String remote_url, String username, String token, String git_name, String git_email) {
+        this.remote_url = remote_url;
+        this.username = username;
+        this.token = token;
+        this.git_name = git_name;
+        this.git_email = git_email;
     }
 
-    @NonNull
+    public int getId() {
+        return id;
+    }
+
     public String getName() {
         return this.name;
     }
+
+    public String getLocal_path() {
+        return local_path;
+    }
+
+    public String getRemote_url() {
+        return this.remote_url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getGit_name() {
+        return git_name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLocal_path(String local_path) {
+        this.local_path = local_path;
+    }
+
+    public String getGit_email() {
+        return git_email;
+    }
+
 }
