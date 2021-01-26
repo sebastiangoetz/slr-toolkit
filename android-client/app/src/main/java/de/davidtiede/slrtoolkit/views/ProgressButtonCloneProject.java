a0b8125585bb0e316689d739e817aefc221db2f9
David Tiede <tidenhub@gmx.de>
2021-01-26 16:38:02 +0100

    Add Progress Button

---
+++ android-client/app/src/main/java/de/davidtiede/slrtoolkit/views/ProgressButtonCloneProject.java
@@ -0,0 +1,43 @@
+package de.davidtiede.slrtoolkit.views;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+
+import de.davidtiede.slrtoolkit.R;
+
+public class ProgressButtonCloneProject {
+
+    private Context context;
+    private CardView cardView;
+    private ProgressBar progressBar;
+    private TextView textView;
+
+    public ProgressButtonCloneProject(Context context, View view) {
+        this.context = context;
+        cardView = view.findViewById(R.id.button_clone_project);
+        progressBar = view.findViewById(R.id.progressbar_clone_project);
+        textView = view.findViewById(R.id.textview_clone_project);
+    }
+
+    void buttonLoading() {
+        progressBar.setVisibility(View.VISIBLE);
+        textView.setText(context.getResources().getString(R.string.please_wait));
+    }
+
+    void buttonSuccessful() {
+        cardView.setBackgroundColor(Color.GREEN);
+        progressBar.setVisibility(View.GONE);
+        textView.setText(context.getResources().getString(R.string.please_wait));
+    }
+
+    void buttonFailure() {
+        cardView.setBackgroundColor(Color.RED);
+        progressBar.setVisibility(View.GONE);
+        textView.setText(context.getResources().getString(R.string.please_wait));
+    }
+}
