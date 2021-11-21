354aec92a5fa16a56ce821068e719fb0bad883e9
Franziska Müller <franzimail94@yahoo.de>
2021-11-21 09:25:38 +0100

    add activity for taxonomy classes

--- android-client/app/src/main/java/de/davidtiede/slrtoolkit/ProjectActivity.java
+++ android-client/app/src/main/java/de/davidtiede/slrtoolkit/ProjectActivity.java
@@ -1,15 +1,12 @@
 package de.davidtiede.slrtoolkit;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import de.davidtiede.slrtoolkit.database.Repo;
 import de.davidtiede.slrtoolkit.viewmodels.ProjectViewModel;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +14,7 @@ import android.widget.Button;
 public class ProjectActivity extends AppCompatActivity {
     private Button allEntryButton;
     private Button filterButton;
+    private Button taxonomyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +25,7 @@ public class ProjectActivity extends AppCompatActivity {
         ProjectViewModel projectViewModel = new ViewModelProvider(this).get(ProjectViewModel.class);
         allEntryButton = findViewById(R.id.button_all_entries);
         filterButton = findViewById(R.id.button_filter);
+        taxonomyButton = findViewById(R.id.button_entries_by_taxonomy);
 
         if(extras != null) {
             int id = extras.getInt("repo");
@@ -49,24 +48,15 @@ public class ProjectActivity extends AppCompatActivity {
 
             //to initialize the database
             /*final Observer projectPathObserver = new Observer<Repo>() {
-                @Override
-                public void onChanged(Repo repo) {
-                    String path = repo.getLocal_path();
-                    projectViewModel.initializeData(repo.getId(), repo.getLocal_path());
-                }
-            };
-            projectViewModel.getRepoById(id).observe(this, projectPathObserver);*/
-
-            //to initialize the database
-            final Observer projectPathObserver = new Observer<Repo>() {
                 @RequiresApi(api = Build.VERSION_CODES.O)
                 @Override
                 public void onChanged(Repo repo) {
                     String path = repo.getLocal_path();
+                    projectViewModel.initializeData(repo.getId(), repo.getLocal_path());
                     projectViewModel.initializeTaxonomy(repo.getId(), repo.getLocal_path());
                 }
             };
-            projectViewModel.getRepoById(id).observe(this, projectPathObserver);
+            projectViewModel.getRepoById(id).observe(this, projectPathObserver);*/
 
             allEntryButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -82,6 +72,16 @@ public class ProjectActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(getApplicationContext(), FilterActivity.class);
+                    intent.putExtra("repo", id);
+                    startActivity(intent);
+                }
+            });
+
+            taxonomyButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), EntriesByTaxonomyActivity.class);
                     intent.putExtra("repo", id);
                     startActivity(intent);
                 }
