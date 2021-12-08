c007e72b3c38dd67b7e45692c43775a205a9419c
Franziska Müller <franzimail94@yahoo.de>
2021-12-08 17:17:28 +0100

    use BibtexEntryDetailFragment in classifyActivity

--- android-client/app/src/main/java/de/davidtiede/slrtoolkit/ClassifyActivity.java
+++ android-client/app/src/main/java/de/davidtiede/slrtoolkit/ClassifyActivity.java
@@ -1,8 +1,10 @@
 package de.davidtiede.slrtoolkit;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
@@ -11,13 +13,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import de.davidtiede.slrtoolkit.database.Entry;
+import de.davidtiede.slrtoolkit.fragments.BibtexEntryDetailFragment;
 import de.davidtiede.slrtoolkit.viewmodels.BibtexEntriesViewModel;
-import de.davidtiede.slrtoolkit.views.EntriesListViewPagerAdapter;
 
-public class ClassifyActivity extends AppCompatActivity {
+public class ClassifyActivity extends FragmentActivity {
     ViewPager2 viewPager;
     BibtexEntriesViewModel bibtexEntriesViewModel;
     int repoId;
+    private FragmentStateAdapter pagerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +37,37 @@ public class ClassifyActivity extends AppCompatActivity {
         }
 
         viewPager = findViewById(R.id.classify_entries_viewpager);
+        pagerAdapter = new ScreenSlidePagerAdapter(ClassifyActivity.this, new ArrayList<Entry>());
+        viewPager.setAdapter(pagerAdapter);
 
         bibtexEntriesViewModel.getEntriesForRepo(repoId).observe(this, new Observer<List<Entry>>() {
             @Override
             public void onChanged(List<Entry> entries) {
-                EntriesListViewPagerAdapter entriesListViewPagerAdapter = new EntriesListViewPagerAdapter((ArrayList<Entry>) entries);
-                viewPager.setAdapter(entriesListViewPagerAdapter);
+                pagerAdapter = new ScreenSlidePagerAdapter(ClassifyActivity.this, entries);
+                viewPager.setAdapter(pagerAdapter);
             }
         });
     }
+
+    private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+        List<Entry> entries;
+
+        public ScreenSlidePagerAdapter(FragmentActivity fa, List<Entry> entries) {
+            super(fa);
+            this.entries = entries;
+        }
+
+        @Override
+        public Fragment createFragment(int position) {
+            Entry entry = entries.get(position);
+            Fragment fragment = BibtexEntryDetailFragment.newInstance(entry.getId());
+            return fragment;
+        }
+
+        @Override
+        public int getItemCount() {
+            return entries.size();
+        }
+    }
+
 }
